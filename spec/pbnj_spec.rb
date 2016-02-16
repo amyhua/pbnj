c765280b555319891bfeabb673903c3d1ee32cf5
Amy Hua <amy@liveapp.com>
2016-02-16 01:21:48 -0500

    Init Sinatra app

---
+++ spec/pbnj_spec.rb
@@ -0,0 +1,14 @@
+require_relative "spec_helper"
+require_relative "../pbnj.rb"
+
+def app
+  Pbnj
+end
+
+describe Pbnj do
+  it "responds with a welcome message" do
+    get '/'
+
+    last_response.body.must_include 'Welcome to the Sinatra Template!'
+  end
+end
